aeb1bbf84243ac3044fcf9cf1d02119bf8c54adc
Steve Krueger <stevekrueger52@gmail.com>
2019-10-28 13:07:47 -0400

    Removed nested folder, as it was messing up the .gitignore (Library folder wasn't being ignored). Should be fixed now

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 delta = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        delta = delta.magnitude > 1 ? delta.normalized : delta;
+        transform.localPosition += new Vector3(delta.x, delta.y, 0) * speed;
+    }
+}
