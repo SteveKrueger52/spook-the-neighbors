b1c683449812b0c028d9e058aaefc696b6dd4fe2
JKosof <joshkosof@gmail.com>
2019-10-29 20:13:01 -0400

    highlight functionality

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class GhostController : MonoBehaviour
 {
     public float speed;
+    public List<GameObject> hauntables = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        hauntables = new List<GameObject>(GameObject.FindGameObjectsWithTag("HauntableObject"));
     }
 
     // Update is called once per frame
@@ -19,6 +20,29 @@ public class GhostController : MonoBehaviour
         transform.localPosition += new Vector3(delta.x, delta.y, 0) * speed;
         if (Input.GetButtonDown("Boo"))
             boo();
+        if (Input.GetButtonDown("Boo"))
+        {
+            //hide
+        }
+
+        if (Input.GetButtonUp("Boo"))
+        {
+            //unhide
+        }
+
+        hauntables.Sort(delegate (GameObject a, GameObject b)
+        {
+            return Vector2.Distance(this.transform.position, a.transform.position)
+            .CompareTo(
+              Vector2.Distance(this.transform.position, b.transform.position));
+        });
+        foreach (GameObject i in hauntables)
+        {
+            HauntableObject obj = i.GetComponent<HauntableObject>();
+            obj.isHighlighted = false;
+        }
+        if (Vector2.Distance(this.transform.position, hauntables[0].transform.position) < 5) //5 is arbitrary range, requires ingame testing
+            hauntables[0].GetComponent<HauntableObject>().isHighlighted = true;
     }
 
     void boo()
