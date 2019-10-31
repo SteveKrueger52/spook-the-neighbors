2ba1b714ee1dd5f1cedb3f93c4a8b0f8773dee8d
Rudra-Trivedi <trivedirudra2@gmail.com>
2019-10-31 04:02:03 -0400

    First Commit
    Patrolling working fine. Working on investigating the trigger

---
+++ Assets/Scripts/AIPatrol.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPatrol : MonoBehaviour
+{
+    [SerializeField]
+    float MinX, MaxX, speed, InitWait;
+    float X, Y,WaitTime;
+
+    List<GameObject> TriggeredObjects;
+
+    Vector2 MoveSpot;
+
+    bool TargetReached;
+ 
+    
+
+    public bool isInvestigating;
+
+    public ObjectTrigger objecttriggerscript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Y = transform.position.y;
+        X = Random.Range(MinX, MaxX);
+        isInvestigating = false;
+        TriggeredObjects = new List<GameObject>();
+        TargetReached = true;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TriggeredObjects.Count > 0)
+        {
+            isInvestigating = true;
+        }
+        if(TriggeredObjects.Count <= 0)
+        {
+            isInvestigating = false;
+        }
+        
+
+        if (!isInvestigating)
+        {
+            Patrol();
+        }
+
+        if(isInvestigating)
+        {
+            Investigate();
+        }
+
+     //   Debug.Log(TriggeredObjects.Count);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "InteractableObject")
+        {
+            
+
+            objecttriggerscript = collision.gameObject.GetComponent<ObjectTrigger>();
+
+            if(objecttriggerscript.isTriggered)
+            {
+                Debug.Log(collision.gameObject.name + " triggered");
+                TriggeredObjects.Add(collision.gameObject);
+            }
+            
+        }
+    }
+    
+
+
+    void Patrol()
+    {
+        MoveSpot = new Vector2(X, Y);
+        transform.position = Vector2.MoveTowards(transform.position, MoveSpot, speed * Time.deltaTime);
+
+        if ((Vector2.Distance(transform.position, MoveSpot)) < 0.2f)
+        {
+            if (WaitTime <= 0)
+            {
+                X = Random.Range(MinX, MaxX);
+                WaitTime = InitWait;
+            }
+
+            else
+            {
+                WaitTime -= Time.deltaTime;
+
+            }
+        }
+    }
+
+    //void Investigate()
+    //{
+    //    if (TargetReached)
+    //    {
+    //        foreach (GameObject item in TriggeredObjects)
+    //        {
+    //            TargetReached = false;
+    //            MoveSpot = new Vector2(item.transform.position.x, Y);
+    //            transform.position = Vector2.MoveTowards(transform.position, MoveSpot, speed * Time.deltaTime);
+    //            if (Vector2.Distance(transform.position, MoveSpot) < 0.2f)
+    //            {
+    //                TargetReached = true;
+    //            }
+    //        }
+    //    }
+
+        
+    //    TriggeredObjects = new List<GameObject>();
+        
+        
+
+    //}
+}
---
+++ Assets/Scripts/ObjectTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTrigger : MonoBehaviour
+{
+    public  bool isTriggered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTriggered = false;
+        Invoke("Trigger", 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Trigger()
+    {
+        isTriggered = true;
+    }
+
+
+}
