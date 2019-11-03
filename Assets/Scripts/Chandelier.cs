9c497602ef9a55dc5bcfe55e21da06a3c4efef10
JKosof <joshkosof@gmail.com>
2019-11-03 16:26:33 -0500

    added chandelier and tv scripts

---
+++ Assets/Scripts/Chandelier.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chandelier : HauntableObject
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnBoo()
+    {
+        //fall and scare
+        Unhaunt();
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Television.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Television : HauntableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
