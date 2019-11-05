1b588f35311bc004db1906de1806102b8d2f4ddd
JKosof <joshkosof@gmail.com>
2019-11-05 03:15:14 -0500

    basic tv and chandelier prefabs

--- Assets/Scripts/Television.cs
+++ Assets/Scripts/Television.cs
@@ -7,7 +7,7 @@ public class Television : HauntableObject
     // Start is called before the first frame update
     void Start()
     {
-        speed = 1;
+
     }
 
     // Update is called once per frame
