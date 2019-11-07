59f5094a2417ad574c00fc97d78dfd2f832d344c
JKosof <joshkosof@gmail.com>
2019-11-07 17:28:46 -0500

    Boo!
    don't really have time to test this before class, but this code assumes all ai behavior stuff  works properly as discussed yesterday

--- Assets/Scripts/Television.cs
+++ Assets/Scripts/Television.cs
@@ -15,4 +15,18 @@ public class Television : HauntableObject
     {
         
     }
+
+    public override void OnBoo()
+    {
+        Instantiate(new ObjectTrigger(), this.transform);
+        GameObject[] people = GameObject.FindGameObjectsWithTag("Person");
+        foreach (GameObject target in people)
+        {
+            float distance = Vector3.Distance(target.transform.position, transform.position);
+            if (distance < 5)//5 is arbitrary range, requires ingame testing
+            {
+                target.GetComponent<Person>().Scare(20);
+            }
+        }
+    }
 }
