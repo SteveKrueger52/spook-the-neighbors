b42144ebe2741fb0e09bdb7ea721f83523cd0df4
Rudra-Trivedi <trivedirudra2@gmail.com>
2019-12-12 17:28:13 -0500

    Cat Prefab

--- Assets/Scripts/CatScript.cs
+++ Assets/Scripts/CatScript.cs
@@ -15,20 +15,54 @@ public class CatScript : MonoBehaviour
 
     public float speed, RunSpeed;
 
+    public List<GameObject> exits = new List<GameObject>();
+
+    public GameObject GhostObject;
+
+    string Status;
+
+    SpriteRenderer rendnew;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         Y = transform.position.y;
         X = Random.Range(MinX, MaxX);
-        
+
+        Status = "idle";
+
+        exits = new List<GameObject>(GameObject.FindGameObjectsWithTag("Exit"));
+
+        rendnew = GetComponent<SpriteRenderer>();
+        rendnew.color = new Color(1, 0, 0);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        idle();
+        if (Status == "idle")
+        {
+            idle();
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(Vector2.Distance(GhostObject.transform.position, transform.position) <= 5)
+            {
+                Status = "gtfo";
+            }
+
+        }
+
+        if(Status=="gtfo")
+        {
+            GTFO();
+        }
+
+
     }
 
     void idle()
@@ -50,6 +84,33 @@ public class CatScript : MonoBehaviour
                 WaitTime -= Time.deltaTime;
 
             }
+        }
+    }
+
+    public GameObject GetClosestExit() //this finds closest exit to person on the screen, not necessarily exit with shortest path
+    {
+        exits.Sort(delegate (GameObject a, GameObject b)
+        {
+            return Vector2.Distance(this.transform.position, a.transform.position)
+            .CompareTo(
+              Vector2.Distance(this.transform.position, b.transform.position));
+        });
+        return exits[0];
+    }
+
+    public void GTFO()
+    {
+
+        MoveSpot = new Vector2(GetClosestExit().transform.position.x,Y);
+        transform.position = Vector2.MoveTowards(transform.position, MoveSpot, RunSpeed * Time.deltaTime);
+
+
+
+        if ((Vector2.Distance(transform.position, MoveSpot)) < 0.2f)
+        {
+
+            gameObject.SetActive(false);
+
         }
     }
 }
